49dbaaec905c84ab14796443737e65889c2e9fe5
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2025-12-01 20:48:03 -0300

    versão com literais e i18n

--- app/src/main/java/br/edu/utfpr/calculaimc/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/calculaimc/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 import kotlin.math.pow
+import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,19 +34,22 @@ class MainActivity : AppCompatActivity() {
 
         init()
 
-        btCalcular.setOnClickListener{
+        btCalcular.setOnClickListener {
             btCalcularOnClick()
         }
 
-        btLimpar.setOnClickListener{
+        btLimpar.setOnClickListener {
             btLimparOnClick()
+        }
+        btLimpar.setOnLongClickListener {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+            true
         }
     } //fim do onCreate()
 
 
-
-    private fun init(){
-        etPeso = findViewById( R.id.etPeso)
+    private fun init() {
+        etPeso = findViewById(R.id.etPeso)
         etAltura = findViewById(R.id.etAltura)
         tvResultado = findViewById(R.id.tvResultado)
         btCalcular = findViewById(R.id.btCalcular)
@@ -54,27 +61,47 @@ class MainActivity : AppCompatActivity() {
         val peso = etPeso.text.toString().toDoubleOrNull()
         val altura = etAltura.text.toString().toDoubleOrNull()
 
-        if (peso == null ) {
-            etPeso.error = "Peso deve ser informado. "
+        if (peso == null) {
+            etPeso.error = getString(R.string.peso_error)
             return
         }
+
         if (altura == null) {
-            etAltura.error = "Altura deve ser informado. "
+            //tost.makeText(this, "altura deve ser informado.", Toast.LENGTH_SHORT).show()
+            etAltura.error = getString(R.string.altura_error)
             return
         }
 
 
         //processamento
-        val resultado = peso / altura.pow( 2.0 )
+        val locale: String = Locale.getDefault().language //recupera a linguagem do dispositivo (en ,pt)
+        //val countryCode : String = Locale.getDefault().country //recupera o país do dispositivo (US, BR)
+        val resultado: Double = calculaimc(peso, altura, locale)
+
 
         //saída
-        tvResultado.text = "%.2f".format( resultado )
+        tvResultado.text = "%.2f".format(resultado)
+        val nf: NumberFormat = NumberFormat.getInstance(Locale.getDefault())
+        tvResultado.text = nf.format(resultado)
+    }
+
+    private fun calculaimc(peso: Double, altura: Double, locale: String): Double {
+
+        if (locale.equals(other = "en", ignoreCase = true)) {
+            return 783 * (peso / altura.pow(x = 2.0))
+        } else {
+            return (peso / altura.pow(x = 2.0))
+
+        }
+
+        val resultado: Double = peso / altura.pow(2.0)
+
     }
 
     private fun btLimparOnClick() {
-        etPeso.setText( "" )
-        etAltura.setText( "" )
-        tvResultado.setText( "" )
+        etPeso.setText("")
+        etAltura.setText("")
+        tvResultado.text = getString(R.string.zeros)
         etPeso.error = null
         etAltura.error = null
         etPeso.requestFocus()
